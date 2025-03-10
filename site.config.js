4f96aea7c0b5deb11b90a19caccc041369a5213c
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-03-11 03:42:56 +0900

    update

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Kyunghyung Kang",
-    // image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app /avatar.svg
     role: "backend developer",
     bio: "기록",
     email: "kangkyunghyun.dev@gmail.com",
